08b92e147b96d0ad58cc365f309d79511ca4ae60
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-04-23 12:17:55 -0700

    initial commit CSS and JS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// declaring a startGame function
+document.getElementById("button").addEventListener("click", myFunction);
+
+function startGame() {
+    document.getElementById("button").innerHTML = "YOU CLICKED ME!";
+  }
