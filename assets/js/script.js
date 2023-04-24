96baaa6cb3f9922943e89d02e2247e3b345ec2da
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-04-24 15:08:16 -0700

    questions added and sections hiding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,58 @@
 // variables
 var startButton = document.querySelector(".start-button");
 var timeEl = document.querySelector(".time");
-var secondsLeft;
-var isWin = false;
+var secondsLeft = 75;
+var startSectionEl = document.querySelector("#start-section");
+var firstQuestionSectionEl = document.querySelector("#first-question-section");
+var secondQuestionSectionEl = document.querySelector("#second-question-section");
+var thirdQuestionSectionEl = document.querySelector("#third-question-section");
+var endSectionEl = document.querySelector("#end-section");
+var questionButtons = document.querySelector(".question-buttons");
+var questions = [
+{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correctAnswer: "alerts"
+},
+{
+    title: "The condition in an if / else statement is enclosed with in ___.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    correctAnswer: "curly brackets"
+},
+{
+    title: "Arrays in JavaScript can be used to store ___.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "all of the above"
+},
+]
+
+firstQuestionSectionEl.setAttribute("class", "hidden");
+secondQuestionSectionEl.setAttribute("class", "hidden");
+thirdQuestionSectionEl.setAttribute("class", "hidden");
+endSectionEl.setAttribute("class", "hidden");
+// The init function is called when the page loads?
 
 // declare a startGame function
-
-
-function startGame() {
-    isWin = false;
-    secondsLeft = 75;
-    startButton.disabled = true;
-    startTimer()
+function firstQuestion() {
+    startSectionEl.setAttribute("class", "hidden");
+    firstQuestionSectionEl.classList.remove("hidden");
+    setTime()
 }
-  
 
-    //   start timer
+function secondQuestion() {
+    firstQuestionSectionEl.setAttribute("class", "hidden");
+    secondQuestionSectionEl.classList.remove("hidden");
+}
+
+function thirdQuestion() {
+    secondQuestionSectionEl.setAttribute("class", "hidden");
+    thirdQuestionSectionEl.classList.remove("hidden");
+}
 
 // declare a startTimer function
 var timeEl = document.querySelector(".time");
 
 function setTime() {
-    // var secondsLeft = 75;
     // Sets interval in variable
     var timerInterval = setInterval(function() {
       secondsLeft--;
@@ -37,20 +68,7 @@ function setTime() {
     }, 1000);
   }
   
-  // Function to create and append colorsplosion image
-//   function sendMessage() {
-//     timeEl.textContent = " ";
-//     var imgEl = document.createElement("img");
-//     imgEl.setAttribute("src", "images/image_1.jpg");
-//     mainEl.appendChild(imgEl);
   
-//   }
-  
-  setTime();
-
-//   Goes to new screen for new question(DOM manipulation)
-
-
 
 // gameDone function called when all questions are answered
 
@@ -63,4 +81,6 @@ function setTime() {
 // save initials (localStorage)
 
 // Attach event listener to start button to call startGame function on click
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", firstQuestion);
+questionButtons.addEventListener("click", secondQuestion); 
+questionButtons.addEventListener("click", thirdQuestion); 
